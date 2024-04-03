ba038d952ce6b2bfc8c4f71ff31ca7274b678dbb
jaszmije <jaszmije@gmail.com>
2024-04-03 08:48:58 +0200

    add module 15.2 unit tests for currency calculator

---
+++ src/utils/convertPLNToUSD.js
@@ -0,0 +1,27 @@
+export const convertPLNToUSD = (PLN) => {
+
+  if (PLN === undefined || PLN === '') {
+    return NaN;
+  }
+
+  if (typeof PLN === 'string') {
+    return NaN;
+  }
+
+  if (PLN === null || (typeof PLN !== 'string' && typeof PLN !== 'number')) {
+    return 'Error';
+  }
+
+  const PLNtoUSD = PLN / 3.5;
+
+  const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD'
+  });
+
+  if (PLN <0) {
+    return formatter.format(0).replace(/\u00a0/g, ' ');
+  }
+
+  return formatter.format(PLNtoUSD).replace(/\u00a0/g, ' ');
+}
